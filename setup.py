cf0c85c42f5562a5b659612e6540aca233e51ba2
Benjamin Ayles <Benjamin@Benjamin-2.local>
2016-10-22 13:30:14 +1030

    added tag

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='AMFBowlingScraper',
-      version='0.1',
+      version='0.0.1',
       description='AMF Bowling Scraper',
       url='http://github.com/parkourben99/AMFBowlingScraper',
       author='Benjamin Ayles',
